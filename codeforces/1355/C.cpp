63f4edf23e692e45230a4fe660da1e6da0d73d73
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-05-18 01:32:00 -0500

    Add solution for problem `C - Count Triangles` Link: https://codeforces.com/contest/1355/problem/C Tags: binary search, implementation, math, two pointers, *1800 Ref: https://codeforces.com/contest/1355/submission/80575179

--- codeforces/1355/C.cpp
+++ codeforces/1355/C.cpp
@@ -16,19 +16,67 @@ typedef long double ld;
 typedef long long lli;
 typedef pair<int,int> ii;
 #define deb(x) cout << #x": " << (x) << endl;
-const int N = 2 * 5e5 + 5;
-lli acu[N];
+const int N = 5e5 + 5;
+struct SegTree{
+  #define mid (l + r) / 2
+  #define left(u) (u + 1)
+  #define right(u) (u + ((mid - l + 1) << 1))
+  struct Node{
+    lli s, lazy;
+    Node(lli s = 0, lli lazy = 0): s(s), lazy(lazy) {}
+    Node operator + (const Node &n){
+      return Node(s + n.s);
+    }
+  };
+  vector<Node> st;
+  SegTree(int n){
+    st.resize(2 * n);
+  }
+  void push(int u, int l, int r){
+    if(st[u].lazy){
+      st[left(u)].s += st[u].lazy;
+      st[right(u)].s += st[u].lazy;
+      st[left(u)].lazy += st[u].lazy;
+      st[right(u)].lazy += st[u].lazy;
+      st[u].lazy = 0;
+    }
+  }
+  void update(int u, int l, int r, int ll, int rr){
+    if(l > r or r < ll or l > rr) return;
+    if(ll <= l and r <= rr){
+      st[u].s += 1;
+      st[u].lazy += 1;
+      return;
+    }
+    push(u, l, r);
+    update(left(u), l, mid, ll, rr);
+    update(right(u), mid + 1, r, ll, rr);
+    st[u] = st[left(u)] + st[right(u)];
+  }
+  lli query(int u, int l, int r, int ll, int rr){
+    if(l > r or r < ll or l > rr) return 0;
+    if(ll <= l and r <= rr) return st[u].s;
+    return query(left(u), l, mid, ll, rr) + query(right(u), mid + 1, r, ll, rr);
+  }
+  void floor(int u, int l, int r){
+    if(l == r) return;
+    push(u, l, r);
+    floor(left(u), l, mid);
+    floor(right(u), mid + 1, r);
+    st[u] = st[left(u)] + st[right(u)];
+  }
+};
 int main(){ _
   lli a, b, c, d; cin >> a >> b >> c >> d;
+  lli n = 2 * N + 5;
+  SegTree st(n);
   fore(i, a, b + 1){
-    acu[i + b]++;
-    acu[i + c + 1]--;
+    st.update(0, 0, n - 1, i + b, i + c);
   }
-  fore(i, 1, N) acu[i] += acu[i - 1];
-  for(int i = N - 2; i >= 0; i--) acu[i] += acu[i + 1];
+  st.floor(0, 0, n - 1);
   lli res = 0;
   fore(i, c, d + 1){
-    res += acu[i + 1];
+    res += st.query(0, 0, n - 1, i + 1, n - 1);
   }
   cout << res << ENDL;
   return 0;
