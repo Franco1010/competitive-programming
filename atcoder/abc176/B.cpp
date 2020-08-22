e19ca63e32c4fb1218ddd781a753b00cfab6e4da
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-08-22 12:46:00 -0500

    Add solution for problem `B - Multiple of 9` Link: https://atcoder.jp/contests/abc176/tasks/abc176_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc176/submissions/16163487

---
+++ atcoder/abc176/B.cpp
@@ -0,0 +1,29 @@
+/*
+ © 2020-08-22 12:40:23 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  string s; cin >> s;
+  lli foo = 0;
+  for(auto i : s) foo += i - '0';
+  cout << (foo % 9 ? "No" : "Yes") << ENDL;
+  return 0;
+}
