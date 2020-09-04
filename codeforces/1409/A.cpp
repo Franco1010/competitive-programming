6753080a28c9fd61323ac67857aa7a607b69833e
Franco1010 <franco.ramirez@alumno.udg.mx>
2020-09-04 09:37:00 -0500

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/91802742

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,30 @@
+/*
+ © 2020-09-04 09:35:02 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  lli t; cin >> t;
+  while(t--){
+    lli a, b; cin >> a >> b;
+    cout << abs(a - b) / 10 + (abs(a - b) % 10 != 0) << ENDL;
+  }
+  return 0;
+}
