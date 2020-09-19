b15cc8eb1239f890c561b36c3a15962d7faef888
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-09-19 07:03:00 -0500

    Add solution for problem `B - Go to Jail` Link: https://atcoder.jp/contests/abc179/tasks/abc179_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc179/submissions/16853287

---
+++ atcoder/abc179/B.cpp
@@ -0,0 +1,38 @@
+/*
+ © 2020-09-19 07:00:13 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  lli n; cin >> n;
+  lli last = 0;
+  fore(i, 0, n){
+    lli x, y; cin >> x >> y;
+    if(x == y){
+      last++;
+    }else last = 0;
+    if(last >= 3){
+      cout << "Yes" << ENDL;
+      return 0;
+    }
+  }
+  cout << "No" << ENDL;
+  return 0;
+}
