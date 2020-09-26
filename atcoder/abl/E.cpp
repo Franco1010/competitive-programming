ad1dc5da16ac728a7813421778de1f5f384d845f
Franco1010 <franco.ramirez@alumno.udg.mx>
2020-09-26 08:22:00 -0500

    Add solution for problem `E - Replace Digits` Link: https://atcoder.jp/contests/abl/tasks/abl_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abl/submissions/17044329

--- atcoder/abl/E.cpp
+++ atcoder/abl/E.cpp
@@ -1,10 +1,11 @@
 /*
- © 2020-09-28 15:06:12 Franco1010 All Rights Reserved
+ © 2020-09-26 07:00:12 Franco1010 All Rights Reserved
 */
 
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 #pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
+// #include <atcoder/all>
 #define f first
 #define s second
 #define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
@@ -14,124 +15,70 @@
 #define sz(s) int(s.size())
 #define ENDL '\n'
 using namespace std;
+// using namespace atcoder;
 typedef long double ld;
 typedef long long lli;
 typedef pair<lli,lli> ii;
 typedef vector<lli> vi;
 #define deb(x) cout << #x": " << (x) << endl;
+const lli N = 2e5 + 5;
 const lli MOD = 998244353;
-typedef struct item *pitem;
-struct item {
-	lli pr,cnt,val;
-	lli sum; // (paramters for range query)
-  lli spot;
-  lli pot;
-//	bool rev;
-  lli add; // (parameters for lazy prop)
-	pitem l,r,f;
-	item(lli val, lli pot): pr(rand()),cnt(1),val(val),l(0),r(0), pot(pot), spot(0), add(0)/*,sum(val),rev(0),add(0)*/ {}
-};
-lli cnt(pitem t){return t?t->cnt:0;}
-lli spot(pitem t){return t? t->spot : 0;}
-void push(pitem it){
-	if(it){
-    if(it->add){
-      it->val = it->add;
-      it->sum = spot(it) * it->add % MOD;
-      if(it->l) it->l->add = it->add;
-      if(it->r) it->r->add = it->add;
-      it->add = 0;
+lli pot[N];
+struct segtree{
+  #define mid (l + r) / 2
+  #define left(u) (u + 1)
+  #define right(u) (u + ((mid - l + 1) << 1))
+  struct node{
+    lli s, z;
+    node(lli s = 0, lli z = 0): s(s), z(z) {}
+    node operator + (const node & n){
+      return node((s + n.s + MOD) % MOD);
     }
-		// if(it->rev){
-		// 	swap(it->l,it->r);
-		// 	if(it->l)it->l->rev^=true;
-		// 	if(it->r)it->r->rev^=true;
-		// 	it->rev=false;
-		// }
-    // if(add){
-    //   int ll = it->f ? cnt(it->f->l) : 0;
-    // }
-		// it->val+=it->add;it->sum+=it->cnt*it->add;
-		// if(it->l)it->l->add+=it->add;
-		// if(it->r)it->r->add+=it->add;
-		// it->add=0;
-	}
-}
-int sum(pitem t){return t?push(t),t->sum:0;}
-void upd_cnt(pitem t){
-	if(t){
-		t->cnt=cnt(t->l)+cnt(t->r)+1;
-    t->spot = (t->pot + spot(t->l) + spot(t->r) + MOD) % MOD;
-    t->sum = (t->val * t->pot) % MOD;
-    t->sum = (t->sum + sum(t->l) + sum(t->r)) % MOD;
-		// t->sum=t->val+sum(t->l)+sum(t->r);
-	}
-}
-void merge(pitem& t, pitem l, pitem r){
-	push(l);push(r);
-	if(!l||!r)t=l?l:r;
-	else if(l->pr>r->pr)merge(l->r,l->r,r),l->r->f=t=l;
-	else merge(r->l,l,r->l),r->l->f=t=r;
-	upd_cnt(t);
-}
-void split(pitem t, pitem& l, pitem& r, int sz){
-	if(!t){l=r=0;return;}
-	push(t);
-	if(sz<=cnt(t->l)){
-		split(t->l,l,t->l,sz);r=t;
-		if(l)l->f=0;
-		if(t->l)t->l->f=t;
-	}
-	else {
-		split(t->r,t->r,r,sz-1-cnt(t->l));l=t;
-		if(r)r->f=0;
-		if(t->r)t->r->f=t;
-	}
-	upd_cnt(t);
-}
-void push_all(pitem t){
-	if(t->f)push_all(t->f);
-	push(t);
-}
-pitem root(pitem t, int& pos){ // get root and position for node t
-	push_all(t);
-	pos=cnt(t->l);
-	while(t->f){
-		pitem f=t->f;
-		if(t==f->r)pos+=cnt(f->l)+1;
-		t=f;
-	}
-	return t;
-}
-void output(pitem t){ // useful for debugging
-	if(!t)return;
-	push(t);
-	output(t->l);
-  cout << "(" << t->val << ',' << t->pot << ") ";
-  output(t->r);
-}
+  };
+  vector<node> st;
+  segtree(int n): st(2 * n) {}
+  void push(int u, int l, int r){
+    if(st[u].z){
+      lli foo = (pot[l] - pot[r + 1] + MOD) % MOD;
+      st[u].s = st[u].z * foo % MOD;
+      if(l < r){
+        st[left(u)].z = st[u].z;
+        st[right(u)].z = st[u].z;
+      }
+      st[u].z = 0;
+    }
+  }
+  void update(int u, int l, int r, int ll, int rr, lli dd){
+    push(u, l, r);
+    if(l > r or r < ll or l > rr) return;
+    if(ll <= l and r <= rr){
+      st[u].z += dd;
+      push(u, l, r);
+      return;
+    }
+    push(u, l, r);
+    update(left(u), l, mid, ll, rr, dd);
+    update(right(u), mid + 1, r, ll, rr, dd);
+    st[u] = st[left(u)] + st[right(u)];
+  }
+};
 int main(){ _
   lli n, q; cin >> n >> q;
-  vector<lli> pp = {1};
-  while(sz(pp) < n) pp.pb(pp.back() * 10ll % MOD);
-  reverse(all(pp));
-  // for(auto i : pot) cout << i << ' '; cout << ENDL;
-  pitem t = 0;
-  fore(i, 0, n){
-    merge(t, t, new item(1, pp[i]));
+  pot[n + 1] = 0;
+  pot[n] = 1;
+  for(lli i = n - 1; i >= 0; i--){
+    pot[i] = pot[i + 1] * 10ll % MOD;
   }
-  // output(t);cout << ENDL;
+  for(lli i = n - 1; i >= 0; i--){
+    pot[i] = (pot[i] + pot[i + 1] + MOD) % MOD;
+  }
+  // fore(i, 0, n + 1) cout << pot[i] << ' '; cout << ENDL;
+  segtree st(n);
+  st.update(0, 1, n, 1, n, 1);
   while(q--){
     lli l, r, d; cin >> l >> r >> d;
-    pitem ll, mm, rr;
-    split(t, mm, rr, r);
-    split(mm, ll, mm, l - 1);
-    mm->add = d;
-    merge(mm, ll, mm);
-    merge(t, mm, rr);
-    cout << sum(t) << ENDL;
-    // cout << cnt(ll) << ' ' << cnt(mm) << ' ' << cnt(rr) << ENDL;
-    // cout << spot(ll) << ' ' << spot(mm) << ' ' << spot(rr) << ENDL;
+    st.update(0, 1, n, l, r, d);
+    cout << st.st[0].s << ENDL;
   }
   return 0;
 }
