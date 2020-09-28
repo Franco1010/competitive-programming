215a08fe282ea01873ad0afd87123ce5cfdb4daf
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-09-27 20:21:00 -0500

    Add solution for problem `A - Copy-paste` Link: https://codeforces.com/contest/1417/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1417/submission/94040006

---
+++ codeforces/1417/A.cpp
@@ -0,0 +1,37 @@
+/*
+ © 2020-09-27 20:15:02 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  lli t; cin >> t;
+  while(t--){
+    lli n, k; cin >> n >> k;
+    vector<lli> v(n);
+    lli res = 0;
+    fore(i, 0, n){
+      cin >> v[i];
+    }
+    sort(all(v));
+    fore(i, 1, n) res += (k - v[i]) / v[0];
+    cout << res << ENDL;
+  }
+  return 0;
+}
