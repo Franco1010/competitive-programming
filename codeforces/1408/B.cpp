e1868340852d13e6248cc7b48578f99527e9e17a
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-09-30 10:17:00 -0500

    Add solution for problem `B - Arrays Sum` Link: https://codeforces.com/contest/1408/problem/B Tags: constructive algorithms, greedy, math, *1400 Ref: https://codeforces.com/contest/1408/submission/94317879

---
+++ codeforces/1408/B.cpp
@@ -0,0 +1,44 @@
+/*
+ © 2020-09-30 09:35:01 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  lli t; cin >> t;
+  while(t--){
+    lli n,k; cin >> n >> k;
+    set<lli> st;
+    fore(i, 0, n){
+      lli x; cin >> x;
+      st.insert(x);
+    }
+    if(k == 1){
+      if(sz(st) == 1) cout << 1 << ENDL;
+      else cout << -1 << ENDL;
+    }else{
+      lli tam = sz(st);
+      tam = max(tam - k, 0ll);
+      // for(auto i : st) cout << i << ' '; cout << ENDL;
+      k--;
+      cout << 1ll + (tam / k + (tam % k != 0)) << ENDL;
+    }
+  }
+  return 0;
+}
