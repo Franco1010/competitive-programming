5075ee9e5ade62aa134bccf216da11c061ccd15d
Franco1010 <ramirez.villa.franco.ariel@gmail.com>
2020-10-20 17:11:00 -0500

    Add solution for problem `C - Dominant Piranha` Link: https://codeforces.com/contest/1433/problem/C Tags: constructive algorithms, greedy, *900 Ref: https://codeforces.com/contest/1433/submission/96190585

---
+++ codeforces/1433/C.cpp
@@ -0,0 +1,46 @@
+/*
+ © 2020-10-20 16:50:02 Franco1010 All Rights Reserved
+*/
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+void solve(){
+  lli n; cin >> n;
+  lli mx = 0;
+  vi v(n); fore(i, 0, n) cin >> v[i], mx = max(mx, v[i]);
+  fore(i, 0, n){
+    if(v[i] == mx){
+      if(i > 0 and v[i - 1] < v[i]){
+        cout << i + 1 << ENDL;
+        return;
+      }
+      if(i + 1 < n and v[i + 1] < v[i]){
+        cout << i + 1 << ENDL;
+        return;
+      }
+    }
+  }
+  cout << -1 << ENDL;
+}
+int main(){ _
+  lli t;cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
