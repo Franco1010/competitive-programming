e3bf880dac5dcbbf15760039dcb39642c2f272c6
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-01-04 08:58:00 -0600

    Add solution for problem `C - Long Jumps` Link: https://codeforces.com/contest/1472/problem/C Tags: dp, graphs, *1100 Ref: https://codeforces.com/contest/1472/submission/103217931

---
+++ codeforces/1472/C.cpp
@@ -0,0 +1,40 @@
+/*
+ © 2021-01-04 08:35:01 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli t; cin >> t;
+  while(t--){
+    lli n; cin >> n;
+    vi v(n); fore(i, 0, n) cin >> v[i];
+    vi dp(n, 0);
+    lli mx = 0;
+    fore(i, 0, n){
+      dp[i] += v[i];
+      if(i + v[i] < n) dp[i + v[i]] = max(dp[i + v[i]], dp[i]);
+      mx = max(mx, dp[i]);
+    }
+    cout << mx << ENDL;
+  }
+  return 0;
+}
