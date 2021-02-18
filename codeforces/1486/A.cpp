48abe9928aff072663eeef8eb844be22d92c742b
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-02-18 10:45:00 -0600

    Add solution for problem `A - Shifting Stacks` Link: https://codeforces.com/contest/1486/problem/A Tags: greedy, implementation Ref: https://codeforces.com/contest/1486/submission/107863760

---
+++ codeforces/1486/A.cpp
@@ -0,0 +1,44 @@
+/*
+ © 2021-02-18 08:35:02 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+void solve(){
+  lli n; cin >> n;
+  lli acu = 0;
+  vi v(n); fore(i, 0, n) cin >> v[i];
+  fore(i, 0, n){
+    acu += v[i];
+    if(acu < i){
+      cout << "NO" << ENDL;
+      return;
+    }
+    acu -= i;
+  }
+  cout << "YES" << ENDL;
+}
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli t; cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
