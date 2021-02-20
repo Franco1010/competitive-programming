2e0fa56b65cd82cb6f90bcc403ab1875e98a8c0c
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-02-20 06:03:00 -0600

    Add solution for problem `A - Star` Link: https://atcoder.jp/contests/abc192/tasks/abc192_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc192/submissions/20287021

---
+++ atcoder/abc192/A.cpp
@@ -0,0 +1,29 @@
+/*
+ © 2021-02-20 06:01:23 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli n; cin >> n;
+  cout << 100 - n%100 << ENDL;
+  return 0;
+}
