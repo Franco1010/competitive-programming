c556d9c6b4957a3b8f08ea8d518d350120098e0f
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-02-27 06:02:00 -0600

    Add solution for problem `A - Discount` Link: https://atcoder.jp/contests/abc193/tasks/abc193_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc193/submissions/20520353

---
+++ atcoder/abc193/A.cpp
@@ -0,0 +1,30 @@
+/*
+ © 2021-02-27 05:57:52 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  ld a, b; cin >> a >> b;
+  cout << fixed << setprecision(3);
+  cout << ld(100) - (b * ld(100)) / a << ENDL;
+  return 0;
+}
