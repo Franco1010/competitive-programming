b30aad3c0917af4567cfe1c917d05f7d2b5cbf41
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-03-28 08:47:00 -0600

    Add solution for problem `A - Odd vs Even` Link: https://atcoder.jp/contests/arc116/tasks/arc116_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc116/submissions/21367449

---
+++ atcoder/arc116/A.cpp
@@ -0,0 +1,34 @@
+/*
+ © 2021-03-28 06:25:50 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli t; cin >> t;
+  while(t--){
+    lli n; cin >> n;
+    if(n%4ll == 0) cout << "Even" << ENDL;
+    else if(n%4ll == 1 or n%4ll == 3) cout << "Odd" << ENDL;
+    else cout << "Same" << ENDL;
+  }
+  return 0;
+}
