d0bc7f0b6999b6ea23839953ffdbd5e20a56fb87
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-04-23 09:42:00 -0500

    Add solution for problem `A - Sum of 2050` Link: https://codeforces.com/contest/1517/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1517/submission/113988933

---
+++ codeforces/1517/A.cpp
@@ -0,0 +1,42 @@
+/*
+ © 2021-04-23 09:35:01 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli t; cin >> t;
+  while(t--){
+    lli n; cin >> n;
+    lli base = 2050;
+    fore(i, 0, 15) base *= 10ll;
+    lli ans = 0;
+    fore(i, 0, 16){
+      lli foo = n / base;
+      ans += foo;
+      n -= foo * base;
+      base /= 10ll;
+    }
+    if(n) cout << -1 << ENDL;
+    else cout << ans << ENDL;
+  }
+  return 0;
+}
