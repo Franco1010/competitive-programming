bccf4c3739eeed87411b01ea09a2f2b95fabe124
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-04-30 22:22:00 -0500

    Add solution for problem `C - Rectangle Cutting` Link: https://atcoder.jp/contests/abc130/tasks/abc130_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc130/submissions/22184450

---
+++ atcoder/abc130/C.cpp
@@ -0,0 +1,30 @@
+/*
+ © 2021-04-30 22:19:51 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli a, b, x, y; cin >> a >> b >> x >> y;
+  cout << fixed << setprecision(9);
+  cout << double(a) * double(b) / double(2) << ' ' << (2*x == a and 2*y == b) << ENDL;
+  return 0;
+}
