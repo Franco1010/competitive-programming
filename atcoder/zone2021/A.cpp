1a77570b1977c0ba007a53a7b9695b8c0c9d9b00
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-05-01 07:13:00 -0500

    Add solution for problem `A - UFO Invasion` Link: https://atcoder.jp/contests/zone2021/tasks/zone2021_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/zone2021/submissions/22204273

---
+++ atcoder/zone2021/A.cpp
@@ -0,0 +1,41 @@
+/*
+ © 2021-05-01 07:01:01 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  string s; cin >> s;
+  string base = "ZONe";
+  lli pos = 0, ans = 0;
+  fore(i, 0, sz(s)){
+    bool yes = true;
+    fore(j, 0, sz(base)){
+      if(i + j >= sz(s) or s[i + j] != base[j]){
+        yes = false;
+        break;
+      } 
+    }
+    if(yes) ans++;
+  }
+  cout << ans << ENDL;
+  return 0;
+}
