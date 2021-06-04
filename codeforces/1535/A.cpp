e23cca184e484e474b38ea67aace20ccec6ca353
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-06-04 09:39:00 -0500

    Add solution for problem `A - Fair Playoff` Link: https://codeforces.com/contest/1535/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1535/submission/118372632

---
+++ codeforces/1535/A.cpp
@@ -0,0 +1,36 @@
+/*
+ © 2021-06-04 09:35:01 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli t; cin >> t;
+  while(t--){
+    vi v(4); for(auto& i : v) cin >> i;
+    vi a = {max(v[0], v[1]), max(v[2], v[3])};
+    sort(all(v));
+    sort(all(a));
+    if(a[0] == v[2] and a[1] == v[3]) cout << "YES" << ENDL;
+    else cout << "NO" << ENDL;
+  }
+  return 0;
+}
