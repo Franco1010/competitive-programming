bc2c53014f1f86284057b8d8cc198ffc8a34d412
Franco1010 <franco.ramirez@alumno.udg.mx>
2021-06-18 11:07:00 -0500

    Add solution for problem `E1 - Erase and Extend (Easy Version)` Link: https://codeforces.com/contest/1537/problem/E1 Tags: binary search, brute force, dp, greedy, hashing, implementation, string suffix structures, strings, two pointers, *1600 Ref: https://codeforces.com/contest/1537/submission/119887257

---
+++ codeforces/1537/E1.cpp
@@ -0,0 +1,43 @@
+/*
+ © 2021-06-18 09:35:01 Franco1010 All Rights Reserved
+*/
+
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define sz(s) int(s.size())
+#define ENDL '\n'
+using namespace std;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+#define deb(x) cout << #x": " << (x) << endl;
+
+int main(){ _
+  // freopen("file.in","r",stdin);
+  // freopen("file.out","w",stdout);
+  lli n, k; cin >> n >> k;
+  string foo = "";
+  string s; cin >> s;
+  string ans = "-1";
+  for(auto i : s){
+    foo += i;
+    string bar = "";
+    lli pos = 0;
+    while(sz(bar) < k){
+      bar.pb(foo[pos]);
+      pos = (pos + 1) % sz(foo);
+    }
+    if(ans == "-1") ans = bar;
+    else ans = min(ans, bar);
+  }
+  cout << ans << ENDL;
+  return 0;
+}
